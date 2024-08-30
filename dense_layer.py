dd1c3573e9eca1f56a10fcc35ecb40c5651b6af0
juma <raisben7@gmail.com>
2024-08-30 16:37:03 -0700

    Add a call function to the dense layer class

--- dense_layer.py
+++ dense_layer.py
@@ -7,3 +7,12 @@ class DenseLayer(tf.keras.layer.Layer):
         # Initailize weights and bias
         self.weights = self.add_weight([input_dim, output_dim])
         self.bias = self.add_weight([1, output_dim])
+
+    def call(self, inputs):
+        # Forward propagate the inputs
+        z = tf.matmul(inputs, self.weights) + self.bias
+
+        # Feed through a non-linear activation
+        output = tf.math.sigmoid(z)
+
+        return output
