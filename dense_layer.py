0d29af6cee035f982c9405b72c41c2e62d0e8189
juma <raisben7@gmail.com>
2024-08-30 18:28:02 -0700

    Calculate binary cross entropy loss

--- dense_layer.py
+++ dense_layer.py
@@ -40,3 +40,8 @@ model = tf.keras.Sequential([
     # .
     tf.keras.layers.Dense(2)
 ])
+
+# Binary Cross Entropy Loss
+y = 'actual value'
+predicted = 'predicted value'
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, predicted))
